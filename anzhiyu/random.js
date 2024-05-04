cb8f8a398231e76d85a19669802667b58896f4a6
AshMiszero <ashmiszero@qq.com>
2024-05-04 14:58:33 +0800

    Site updated: 2024-05-04 14:58:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/01/test/","2024/05/04/CPA_AES密码芯片能量分析攻击/"];function toRandomPost(){
+var posts=["2024/05/04/CPA_AES密码芯片能量分析攻击/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
