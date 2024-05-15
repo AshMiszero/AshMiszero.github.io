58ef7fe823b318dd8f2cfd4139a6504a57c88132
AshMiszero <ashmiszero@qq.com>
2024-05-15 23:18:57 +0800

    Site updated: 2024-05-15 23:18:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/04/CPA_相关能量分析攻击/"];function toRandomPost(){
+var posts=["2024/05/04/CPA_相关能量分析攻击/","2024/05/15/遗传算法求解最短路径/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
